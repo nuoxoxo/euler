cf04c7f3da9f676a52a02ccfc8404f1d83776fdd
malcolmnuoxu <49533950+malcolmnuoxu@users.noreply.github.com>
2021-03-18 01:04:41 +0100

    .

--- euler_004.py
+++
@@ -1,8 +0,0 @@
-import math
-arr = []
-for x in range(999, 99, -1):
-    for y in range(x, 99, -1):
-        n = x * y
-        if str(n) == ''.join(reversed(str(n))):
-            arr.append(n)
-print(max(arr))
