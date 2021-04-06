92ab48a32260463ddabac5e8a88114a905ce740b
malcolmnuoxu <49533950+malcolmnuoxu@users.noreply.github.com>
2021-04-06 19:00:03 +0200

    Update e004.c

--- e004.c
+++ e004.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
+
 int main(void){
-int i = 999;
-int j, num, rem, rev, tmp;
-int res = 0;
-while (i > 99)
-{
-    j = i;
-    while (j > 99)
+
+    int i = 999;
+    int j, num, rem, rev, tmp;
+    int res = 0;
+
+    while (i > 99)
     {
-        num = i * j;
-        tmp = num;
-        rem = rev = 0;
-        while (tmp > 0)
+        j = i;
+        while (j > 99)
         {
-            rem = tmp % 10;
-            rev = rev * 10 + rem;
-            tmp /= 10;
+            num = i * j;
+            tmp = num;
+            rem = rev = 0;
+            while (tmp > 0)
+            {
+                rem = tmp % 10;
+                rev = rev * 10 + rem;
+                tmp /= 10;
+            }
+            if (rev == num && res < num)
+            {
+                res = num;
+            }
+            j--;
         }
-        if (rev == num && res < num)
-        {
-            res = num;
-        }
-        j--;
+        i--;
     }
-    i--;
-}
-printf("%d", res);
+    printf("%d", res);
 }
 
